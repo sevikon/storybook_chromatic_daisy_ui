e0e2ab7ec86258f4a2def71a55884444ae8181c0
sevikon <sevikon@gmail.com>
2024-08-23 18:06:18 +0200

    -> fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,28 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {rules: {
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off"
-    }}
+    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+    {languageOptions: {globals: globals.browser}},
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            "react/prop-types": "off",
+            "react/react-in-jsx-scope": "off"
+        }
+    },
+    {
+        ignores: [
+            // for non-global ignores a full glob pattern is required
+            '.next/**/*',
+            '/.next/**/*',
+        ]
+    }, {
+        "settings": {
+            "react": {
+                "pragma": "React",
+                "version": "detect"
+            },
+        }
+    }
 ];
